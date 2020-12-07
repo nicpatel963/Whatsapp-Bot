1f0f8b8a0cedb85abd509f8eae2c1ac29d48515c
nicpate963 <nicpate963@gmail.com>
2020-12-07 11:19:00 +0530

    dialogflow agent added

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 
+from utils import fetch_reply
 app = Flask(__name__)
 
 @app.route("/")
@@ -12,12 +13,13 @@ def sms_reply():
     """Respond to incoming calls with a simple text message."""
     # Fetch the message
     msg = request.form.get('Body')
-
+    phone_no=request.form.get('From')
+    reply=fetch_reply(msg,phone_no)
     # Create reply
     resp = MessagingResponse()
-    resp.message("You said: {}".format(msg))
+    resp.message(reply)
 
-    return str(resp)
+    return str(resp)	
 
 if __name__ == "__main__":
     app.run(debug=True)
